e02ec748ab9f238bcbaafcb4bd93899c1f949f8a
Arif Babayev <arifrb@code.edu.az>
2023-02-15 00:36:43 +0400

    GetAll and GetById

--- WebAPI/Controllers/ProductController.cs
+++ WebAPI/Controllers/ProductController.cs
@@ -26,14 +26,40 @@ namespace WebAPI.Controllers
             _productService = productService;
         }
 
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
             //Dependency Chain
             var result = _productService.GetAll();
-            return result.Data;
 
-            
-            
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);            
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Post(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result);
         }
     }
 }
