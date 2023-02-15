5a2925f2125404a40cad180f915ab62342fe02d6
Arif Babayev <arifrb@code.edu.az>
2023-02-15 21:43:35 +0400

    Fluent Validation

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -3,13 +3,11 @@ namespace Business.Constants
 {
 	public static class Messages
 	{
-		public static string ProductAdded = "Product Added";
+        public static string ProductAdded = "Product Added";
         public static string ProductNameInvalid = "Product Name is Invalid";
         public static string ProductsListed= "All Products Listed";
         public static string MaintanceTime = "Maintance Time";
-
-
-
+        public static string UnitPriceInvalid = "Product's Unit Price is Invalid!";
     }
 }
 
