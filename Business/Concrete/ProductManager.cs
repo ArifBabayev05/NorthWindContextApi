66bd64260f12ec07d68e908d40564527079394f3
Arif Babayev <arifrb@code.edu.az>
2023-02-15 22:26:39 +0400

    Validation rules

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -22,7 +22,14 @@ namespace Business.Concrete
         public IResult Add(Product product)
         {
             //business codes
+            //validation
+
              _productDal.Add(product);
+
+            if (product.UnitPrice <= 0)
+            {
+                return new ErrorResult(Messages.UnitPriceInvalid);
+            }
             if(product.ProductName.Length < 2)
             {
                 return new ErrorResult(Messages.ProductNameInvalid);
