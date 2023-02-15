a7db9c3253309137be6f62f1c565495c8a3e58d6
Arif Babayev <arifrb@code.edu.az>
2023-02-15 21:29:02 +0400

    Using AutoFac IoC

--- Business/DependencyResolvers/AutoFac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/AutoFac/AutofacBusinessModule.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
 namespace Business.DependencyResolvers.AutoFac
 {
-	public class AutofacBusinessModule
+	public class AutofacBusinessModule : Module
 	{
-		public AutofacBusinessModule()
-		{
-		}
-	}
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+             
+        }
+    }
 }
 
