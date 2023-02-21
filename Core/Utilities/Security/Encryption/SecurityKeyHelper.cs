a6712e6c2d3789b186e7d2a424829fc1bd84330e
Arif Babayev <arifrb@code.edu.az>
2023-02-21 12:04:45 +0400

    Security Key Added

---
+++ Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Core.Utilities.Security.Encryption
+{
+	public class SecurityKeyHelper
+	{
+		public static SecurityKey CreateSecurityKey(string securityKey)
+		{
+			return SecurityKey();
+		}
+	}
+}
+
