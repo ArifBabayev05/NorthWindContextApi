4681974a7df4032719793e494a0c77dfbdd62b10
Arif Babayev <arifrb@code.edu.az>
2023-02-21 12:17:25 +0400

    JWT AccesToken

---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Core.Utilities.Security.JWT
+{
+	public class AccessToken : ITokenHelper
+	{ 
+		public string Token { get; set; }
+		public string Expiration { get; set; }
+	}
+}
+
